b5a811edc0d9c54f0d055642809689144f597a6e
alexeyasvarisch <Asvarischalex@gmail.com>
2020-10-13 21:16:52 +0300

    Second commit with Enter btn

--- todo.js
+++ todo.js
@@ -1,51 +1,42 @@
+/// <reference path="../typings/globals/jquery/index.d.ts" />
 const root = document.getElementById('root');
+const title = document.createElement('h1');
+title.append(document.createTextNode('To Do'));
+title.classList.add('h1');
+const label = document.createElement('label');
+const inputItem = document.createElement('input');
+inputItem.setAttribute('type', 'text');
+inputItem.setAttribute('placeholder', 'new item');
+inputItem.setAttribute('autofocus', 'true');
+label.append(document.createTextNode('Add new item: '), inputItem);
+const addButton = document.createElement('button');
+addButton.append(document.createTextNode('Add item'));
 const todoList = document.createElement('ol');
-root.insertAdjacentElement('afterend', todoList);
-//let index = 1;
-const indexArray = [];
-
-// header line
-const applNameText = document.createElement('p');
-applNameText.append(document.createTextNode('To-do'));
-// text
-const addNewItemText = document.createTextNode('Add new Item:\n');
-// input field
-const input = document.createElement('input');
-input.setAttribute('type', 'text');
-// button
-const addButton = document.createElement('addButton');
-const addButtonText = document.createTextNode('Add Item: ');
-addButton.append(addButtonText);
-addButton.classList.add('text');
-// appending dom
-root.append(applNameText, addNewItemText, input, addButton);
-
-addButton.onclick = (event) => {
-    const li = document.createElement('li');
-    const text = event.target.previousSibling.value;
-    const content = document.createTextNode(text);
-    const doneButton = document.createElement("doneButton");
-    const donebuttonText = document.createTextNode("done");
-    doneButton.append(donebuttonText);
-    li.append(content, doneButton);
-    event.target.parentElement.nextSibling.append(li);
-    event.target.previousSibling.value = '';
-    //index++;
+root.append(title, label, addButton, todoList);
+addButton.onclick = function () {
+    const text = inputItem.value.trim();
+    if (text) {
+        const li = document.createElement('li');
+        const delButton = document.createElement('button');
+        delButton.append(document.createTextNode('X'));
+        delButton.classList.add('del');
+        delButton.onclick = function (event) {
+            // todoList.removeChild(event.target.parentElement);
+            event.target.parentElement.remove();
+        }
+        li.append(document.createTextNode(text), delButton);
+        todoList.append(li);
+    }
+    inputItem.value = '';
 }
 
-input.onkeyup = (event) => {
-    event.target.value = event.target.value.toUpperCase();
+inputItem.onkeyup = () => {
+    inputItem.value = inputItem.value.toUpperCase();
 }
 
-doneButton.onclick = (event) => {
-    // console.log(event);
-    todoList.removeChild(event.target.parentElement);
-    
-}
-
-
-
-
-// function eventLogger(event) {
-//     console.log(event.target, event.type);
-// }
+inputItem.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addButton.click();
+    }
+});
